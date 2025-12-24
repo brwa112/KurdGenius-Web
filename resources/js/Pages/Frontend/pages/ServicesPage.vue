b7b0af31b73f958dc88d84c70c05270321a5959f
brwa112 <brwadarbany550@gmail.com>
2025-12-24 15:42:40 +0300

    image

--- resources/js/Pages/Frontend/pages/ServicesPage.vue
+++ resources/js/Pages/Frontend/pages/ServicesPage.vue
@@ -2,22 +2,24 @@
     <div class="relative container 2xl:max-w-[100rem] mx-auto z-20 py-36">
         <div
             class="w-full flex flex-col sm:flex-row items-center justify-center gap-y-5 gap-x-5 xl:gap-x-16 2xl:gap-x-24 text-white lg:px-2">
-            <h1 v-motion-slide-visible-bottom :duration="300" class="font-audi text-xl md:text-2xl xl:text-3xl ms-3">SERVICES</h1>
+            <h1 v-motion-slide-visible-bottom :duration="300" class="font-audi text-xl md:text-2xl xl:text-3xl ms-3">
+                SERVICES</h1>
 
             <div class="w-full grid grid-cols-1 lg:grid-cols-2 gap-y-3 sm:gap-y-5 gap-x-5 xl:gap-x-16 2xl:gap-x-24 p-6">
                 <!-- Card -->
                 <div v-motion-slide-visible-bottom :duration="300" v-for="service in services" :key="service.id"
-                    class="services-card w-full group relative overflow-hidden text-white p-5 sm:p-6 lg:p-8 xl:p-10" data-tilt
-                    data-tilt-scale="1.02" data-tilt-reset-to-start="true" data-tilt-reset="true" data-tilt-speed="1000"
-                    data-tilt-max="5">
-                    <div class="relative z-10 flex gap-1">
-                        <div class="block">
-                            <img :src="service.logo" class="size-9 2xl:size-12 text-white me-3" />
+                    class="services-card w-full group relative overflow-hidden text-white p-5 sm:p-6 lg:p-8 xl:p-10"
+                    data-tilt data-tilt-scale="1.02" data-tilt-reset-to-start="true" data-tilt-reset="true"
+                    data-tilt-speed="1000" data-tilt-max="5">
+                    <div class="relative z-10 flex gap-3">
+
+                        <div class="shrink-0 w-9 h-9 2xl:w-16 2xl:h-16 flex items-center justify-center">
+                            <img :src="service.logo" class="w-full h-full object-contain" />
+                            {{ service.logo }}
                         </div>
                         <div class="flex flex-col justify-center gap-1">
                             <h2 class="text-base sm:text-lg 2xl:text-xl font-medium">{{ service.name }}</h2>
-                            <p
-                                class="text-white block text-xs 2xl:text-sm font-light text-justify leading-tight">
+                            <p class="text-white block text-xs 2xl:text-sm font-light text-justify leading-tight">
                                 {{ service.description }}
                             </p>
                         </div>
